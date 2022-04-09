13a12be7947d4acefdddc9ad3ce5b4734a316a31
Ciberian <isi04047891@gmail.com>
2022-04-09 15:16:29 +0500

    Добавляет вспомогательные функции в модуль util.js и создаёт модуль api.js

---
+++ js/api.js
@@ -0,0 +1,17 @@
+import { showErrorMessage } from './util.js';
+
+const URL_FOR_GET = 'https://25.javascript.pages.academy/kekstagram/data';
+const URL_FOR_SEND = 'https://25.javascript.pages.academy/kekstagram';
+const MESSAGE_TEXT = 'Ошибка при получении данных с сервера, повторите попытку позже.';
+const ALERT_SHOW_TIME = 5000;
+
+showErrorMessage(MESSAGE_TEXT, ALERT_SHOW_TIME);
+
+const getData = () =>
+  fetch(URL_FOR_GET)
+    .then((response) => response.json())
+    .catch(() => showErrorMessage());
+
+const sendData = (data) => fetch(URL_FOR_SEND, { method: 'POST', body: data }).catch();
+
+export { getData, sendData };
--- js/util.js
+++ js/util.js
@@ -1,3 +1,35 @@
 const checkStingLenght = (str, maxLength) => str.length <= maxLength;
 
-export { checkStingLenght };
+const showErrorMessage = (message, showTime) => {
+
+  const alertContainer = document.createElement('div');
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.zIndex = 100;
+  alertContainer.style.top = 0;
+  alertContainer.style.left = 0;
+  alertContainer.style.right = 0;
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, showTime);
+};
+
+const debounce = (callback, timeoutDelay = DEFAULT_DEBOUNCE_DELAY) => {
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+export { checkStingLenght, showErrorMessage, debounce };
