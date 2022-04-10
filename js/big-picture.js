015582c6341a06345b97d9862608a0526db8f0b5
Ciberian <isi04047891@gmail.com>
2022-04-10 16:31:27 +0500

    создаёт функцию отображения дополнительных комментариев в модуле big-picture.js

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,10 +1,15 @@
+const DISPLAYED_COMMENTS = 5;
+const COMMENT_INCREASE_STEP = 5;
+
 const body = document.querySelector('body');
 const bigPicture = body.querySelector('.big-picture');
 const closeBtn = bigPicture.querySelector('#picture-cancel');
 const image = bigPicture.querySelector('img');
 const likes = bigPicture.querySelector('.likes-count');
-const commentsCount = bigPicture.querySelector('.comments-count');
+const commentsTotal = bigPicture.querySelector('.comments-total');
+const commentsShown = bigPicture.querySelector('.comments-shown');
 const socialComments = bigPicture.querySelector('.social__comments');
+const commentsLoader = bigPicture.querySelector('.comments-loader');
 const description = bigPicture.querySelector('.social__caption');
 
 const getDataById = (pictureNode, receivedData) => {
@@ -46,28 +51,56 @@ const closePictureByKeydown = (evt) => {
   }
 };
 
-const showBigPicture = (pictureNode, recievedData) => {
+const showMoreComments = (comments) => {
+  const sliceStart = Number(commentsShown.textContent);
+  let sliceEnd;
+  if (comments.length <= sliceStart + COMMENT_INCREASE_STEP) {
+    sliceEnd = comments.length;
+    commentsLoader.classList.add('hidden');
+  } else {
+    sliceEnd = sliceStart + COMMENT_INCREASE_STEP;
+  }
+  commentsShown.textContent = sliceEnd;
 
-  const pictureData = getDataById(pictureNode, recievedData);
+  comments
+    .slice(sliceStart, sliceEnd)
+    .forEach((comment) => {
+      socialComments.insertAdjacentElement('beforeend', createComment(comment));
+    });
+};
+
+const showBigPicture = (pictureNode, receivedData) => {
+  const pictureData = getDataById(pictureNode, receivedData);
 
   body.classList.add('modal-open');
   bigPicture.classList.remove('hidden');
 
   image.src = pictureData.url;
   likes.textContent = pictureData.likes;
-  commentsCount.textContent = pictureData.comments.length;
   description.textContent = pictureData.description;
+  commentsTotal.textContent = pictureData.comments.length;
+
+  if (pictureData.comments.length < DISPLAYED_COMMENTS) {
+    commentsShown.textContent = commentsShown.textContent.replace(/[0-99]/, pictureData.comments.length);
+    commentsLoader.classList.add('hidden');
+  } else {
+    commentsShown.textContent = commentsShown.textContent.replace(/[0-9]+/g, 5);
+    commentsLoader.classList.remove('hidden');
+  }
 
   while (socialComments.firstChild) {
     socialComments.removeChild(socialComments.firstChild);
   }
 
-  pictureData.comments.forEach((comment) => {
-    socialComments.insertAdjacentElement('afterbegin', createComment(comment));
-  });
+  pictureData.comments
+    .slice(0, DISPLAYED_COMMENTS)
+    .forEach((comment) => {
+      socialComments.insertAdjacentElement('beforeend', createComment(comment));
+    });
 
   closeBtn.addEventListener('click', closePictureByClick);
   document.addEventListener('keydown', closePictureByKeydown);
+  commentsLoader.addEventListener('click', () => showMoreComments(pictureData.comments));
 };
 
 export { showBigPicture };
